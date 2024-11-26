0ec03c6c58415c466ec41142c62ca89cefe5d3a4
Shohzod Irgas <shohzod_irgas@epam.com>
2024-11-26 14:50:56 +0300

    New unit tests have been added.

---
+++ TodoAppUnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using ToDoApp.Models;
+
+namespace TodoAppUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void IsComplete_DefaultValue_ShouldBeTrue()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.IsComplete = true;
+
+            // Act & Assert
+            Assert.True(todoItem.IsComplete);
+        }
+
+        [Fact]
+        public void Name_Value_IsNotNull()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.Name = "Complete ASP.NET Project Work";
+
+            // Act & Assert
+            Assert.NotEmpty(todoItem.Name);
+        }
+
+        [Fact]
+        public void Name_Value_IsNull()
+        {
+            // Arrange
+            var todoItem = new TodoItem();
+            todoItem.Name = String.Empty;
+
+            // Act & Assert
+            Assert.Empty(todoItem.Name);
+        }
+    }
+}
